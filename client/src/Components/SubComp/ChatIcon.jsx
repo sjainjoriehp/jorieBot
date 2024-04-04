bec0027b3707b67f20453234c764397d00b04a11
Sanat Jain <sjain1@joriehp.com>
2024-04-04 19:18:22 +0530

    Change in BG

--- client/src/Components/SubComp/ChatIcon.jsx
+++ client/src/Components/SubComp/ChatIcon.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import JorieBot from '../../resources/JorieBot.png'
+
 const ChatIcon = () => {
     const [ChangeVisibility, setChangeVisibility] = useState('none');
     // const BotImgLable = "Welcome to OrthoOne, Click to get started.";
@@ -10,10 +11,19 @@ const ChatIcon = () => {
     const RemoveElement = () => {
         setChangeVisibility('none');
     }
+
+    const ImageChanger=()=>{
+           
+        console.log("ImageChangerCalled")
+        // var cols = document.getElementsByClassName('col1');
+        // cols.style.backgroundImage  = url('../')
+    }
+     
+
     return (
         <>
             <div style={{ display: ChangeVisibility }} className="greetingBox" onMouseOver={AddElement} onMouseOut={RemoveElement}>{BotImgLable}</div>
-            <img className="botImgLb" src={JorieBot} onMouseOver={AddElement} onMouseOut={RemoveElement} alt='Chat Icon' />
+            <img className="botImgLb" src={JorieBot} onClick={ImageChanger} onMouseOver={AddElement} onMouseOut={RemoveElement} alt='Chat Icon' />
         </>
     );
 };
