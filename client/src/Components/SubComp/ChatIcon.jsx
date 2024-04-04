ad7dcb9276708c51bcccfc0bd32c964377b173bc
Sandeep Negi <snegi@joriehp.com>
2024-04-04 19:16:26 +0530

    commit

--- client/src/Components/SubComp/ChatIcon.jsx
+++ client/src/Components/SubComp/ChatIcon.jsx
@@ -1,19 +1,18 @@
 import React, { useState } from "react";
 import JorieBot from '../../resources/JorieBot.png'
 const ChatIcon = () => {
-    const [ChangeVisibility, setChangeVisibility] = useState('none');
-    // const BotImgLable = "Welcome to OrthoOne, Click to get started.";
+    const [ChangeVisibility,setChangeVisibility] = useState('none');
     const BotImgLable = "Your OrthOne assitant is here to help you!";
-    const AddElement = () => {
+    const AddElement = () =>{
         setChangeVisibility('block');
     }
-    const RemoveElement = () => {
+    const RemoveElement = ()=>{
         setChangeVisibility('none');
     }
     return (
         <>
-            <div style={{ display: ChangeVisibility }} className="greetingBox" onMouseOver={AddElement} onMouseOut={RemoveElement}>{BotImgLable}</div>
-            <img className="botImgLb" src={JorieBot} onMouseOver={AddElement} onMouseOut={RemoveElement} alt='Chat Icon' />
+        <div style={{display:ChangeVisibility}} className="greetingBox" onMouseOver={AddElement} onMouseOut={RemoveElement}>{BotImgLable}</div>
+       <img  className="botImgLb"   src={JorieBot} onMouseOver={AddElement} onMouseOut={RemoveElement} alt='Chat Icon'/>
         </>
     );
 };
