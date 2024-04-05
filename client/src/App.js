1e6a1e3cf6b3d04553506326fe115f76bdc40ed4
Sandeep Negi <snegi@joriehp.com>
2024-04-05 18:00:52 +0530

    commit for font awsom

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,8 @@ import './App.css';
 import JBot from './Components/JBot';
 import { ContextProvider } from './Context/ContextProvider';
 import backgroundImg from './resources/background.png'
-
+import '../node_modules/font-awesome/css/font-awesome.min.css'; 
+import 'font-awesome/css/font-awesome.min.css';
 function App() {
 
   return (
--- client/src/Components/SubComp/CustomService.jsx
+++ client/src/Components/SubComp/CustomService.jsx
@@ -1,15 +1,29 @@
 import React from "react";
 import CallCenterAvtar from "../../resources/CallCenterAvatar.png"
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { 
+    faCheckSquare, faCoffee,faHeadphones,faUserSecret,
+    faCloudUploadAlt, faUserHeadset,faUserNinja,faUserMinus
+} from '@fortawesome/fontawesome-free-solid'
 export const BookApointmentBtn = (props) => {
     const BookApt = () => {
         props.triggerNextStep({ trigger: 'AskName' });
     }
     return (<>
         <div className="Btn_img_aptBook">
-                <button className="Appointment_btn left" onClick={BookApt}>Book an Appointment</button>
-                 <span><img className="call_center_avtar right" src={CallCenterAvtar} /> </span>
+            <button className="Appointment_btn left" onClick={BookApt}>Book an Appointment</button>
+            {/* <span><img className="call_center_avtar right" src={CallCenterAvtar} /> </span> */}
+            <FontAwesomeIcon icon={faUserSecret} />
+            <FontAwesomeIcon icon={faCloudUploadAlt} />
         </div>
+
+    </>);
+}
+
+
+export const GifGreatAfterBtn = (props) => {
+
+    return (<>
 
     </>);
 }
