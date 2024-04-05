d0a491a11cf4bb1152354424bf54cfdf4348bdc6
Sandeep Negi <snegi@joriehp.com>
2024-04-05 15:09:23 +0530

    commit for call center avatar and button by sandeep

--- client/src/Components/SubComp/CustomService.jsx
+++ client/src/Components/SubComp/CustomService.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import CallCenterAvtar from "../../resources/CallCenterAvatar.png"
 
 export const BookApointmentBtn = (props) => {
-    const BookApt = () =>{
-        props.triggerNextStep({ trigger: 'AskName' }); 
+    const BookApt = () => {
+        props.triggerNextStep({ trigger: 'AskName' });
     }
     return (<>
-        <button className="Appointment_btn" onClick={BookApt}>Book an Appointment</button>
-        <span><img className="call_center_avtar" src={CallCenterAvtar}/></span>
+        <div className="Btn_img_aptBook">
+                <button className="Appointment_btn left" onClick={BookApt}>Book an Appointment</button>
+                 <span><img className="call_center_avtar right" src={CallCenterAvtar} /> </span>
+        </div>
+
     </>);
 }
