611b459f0181eb4d44321af475b97cdab5e8a153
Sandeep Negi <snegi@joriehp.com>
2024-04-05 14:44:20 +0530

    commit by sandeep

--- client/src/Components/SubComp/CustomService.jsx
+++ client/src/Components/SubComp/CustomService.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-
+import CallCenterAvtar from "../../resources/CallCenterAvatar.jpg"
 export const BookApointmentBtn = (props) => {
     const BookApt = () =>{
         props.triggerNextStep({ trigger: 'AskName' }); 
     }
     return (<>
-
-        <button className="Appointment_btn" onClick={BookApt}>Book An Appointment</button>
+        <button className="Appointment_btn" onClick={BookApt}>Book an Appointment</button>
+        <span><img className="call_center_avtar" src={CallCenterAvtar}/></span>
     </>);
 }
